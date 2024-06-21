3e190c3440b6b86d0a09ad8be5f8a40ce6475d3f
kartiksam <kartiksam123489@gmail.com>
2024-06-22 01:22:38 +0530

    sservices built

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Service from "./components/Services/Service";
+import MyWork from "./components/Mywork/MyWork";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Navbar></Navbar>
       <Hero></Hero>
       <About />
+      <Service />
+      <MyWork></MyWork>
     </div>
   );
 };
---
+++ frontend/src/components/Mywork/MyWork.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./MyWork.css";
+const MyWork = () => {
+  return <div className="my-work"></div>;
+};
+
+export default MyWork;
