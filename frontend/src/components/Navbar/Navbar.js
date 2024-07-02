560ec5d1e7df02ecdb3c82e78cd8c77d93b55cf1
kartiksam <kartiksam123489@gmail.com>
2024-07-02 16:09:47 +0530

    half responsive

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.svg";
 import underline from "../../assets/nav_underline.svg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
 // for scrolling react anchor libracry and for link to for routing
 // now add functionlaity for contct using web 3 dorm
 const Navbar = () => {
   // to create the underline effect
   const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+  };
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
   return (
     <div className="navbar">
       <img src={logo} alt="" />
-      <ul className="menu">
+      <img src={menu_open} onClick={openMenu} alt="" className="nav-mob-open" />
+      <ul ref={menuRef} className="menu">
+        <img
+          src={menu_close}
+          onClick={closeMenu}
+          alt=""
+          className="nav-mob-close"
+        />
         <li>
           <AnchorLink className="anchor-link" href="#home">
             {" "}
